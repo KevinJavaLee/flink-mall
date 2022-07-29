5ff377f4fecc91a9114ce39b9379191a7f14128c
xiaowenli <xiaowenli@kingsoft.com>
2022-07-29 19:28:32 +0800

    feat(realtime-data-warehouse): 退单信息事实类

---
+++ realtime-data-warehouse/src/main/java/cn/vinlee/bean/OrderInfoRefundBean.java
@@ -0,0 +1,18 @@
+package cn.vinlee.bean;
+
+import lombok.Data;
+
+/**
+ * @author Vinlee Xiao
+ * @className OrderInfoRefundBean
+ * @date 2022/7/25 22:33:32
+ **/
+@Data
+public class OrderInfoRefundBean {
+
+    private String id;
+
+    private String provinceId;
+
+    private String old;
+}
